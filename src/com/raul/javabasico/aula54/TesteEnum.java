972ed7c1f9543f4f843e428d1bbaa2f17307aa00
Raul Correia <raulvinicius245@gmail.com>
2021-09-19 22:21:43 -0300

    atribuindo valores para um Enum.

---
+++ src/com/raul/javabasico/aula54/TesteEnum.java
@@ -0,0 +1,13 @@
+package com.raul.javabasico.aula54;
+
+public class TesteEnum {
+
+	public static void main(String[] args) {
+		
+		DiaSemana dia = DiaSemana.DOMINGO;
+		
+		System.out.println(dia+" - "+dia.getValor());
+
+	}
+
+}
