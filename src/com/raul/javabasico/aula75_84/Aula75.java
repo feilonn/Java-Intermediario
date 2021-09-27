90c782af6b3c5be8aa0c2f6b9f7b329a5ff5dc56
Raul Correia <raulvinicius245@gmail.com>
2021-09-26 22:32:07 -0300

    Iniciando estudos sobre Strings: Concatenação.

---
+++ src/com/raul/javabasico/aula75_84/Aula75.java
@@ -0,0 +1,15 @@
+package com.raul.javabasico.aula75_84;
+
+public class Aula75 {
+
+	public static void main(String[] args) {
+		
+		String resultado = "Resultado 2+2 = " +(2+2); // parentesis da prioridade a expressão matemática
+		String resultado2 = "Resultado 2+2 = " + 2 + 2; // concatena os inteiros como string
+		
+		System.out.println(resultado);
+		System.out.println(resultado2);
+
+	}
+
+}
