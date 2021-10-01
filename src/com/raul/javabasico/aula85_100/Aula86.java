c1df099721014055d76fbcda75910c5dd4080c30
Raul Correia <raulvinicius245@gmail.com>
2021-09-30 22:32:43 -0300

    Utilizando bibliotecas: Date, getDate(), getDay(), getTime().

---
+++ src/com/raul/javabasico/aula85_100/Aula86.java
@@ -0,0 +1,18 @@
+package com.raul.javabasico.aula85_100;
+
+import java.util.Date;
+
+public class Aula86 {
+
+	public static void main(String[] args) {
+		
+		Date hoje = new Date();
+		System.out.println(hoje);
+		
+		System.out.println("Milisegundos desde 1 de Janeiro de 1970 "+hoje.getTime());
+		System.out.println(hoje.getDate());
+		System.out.println(hoje.getDay());
+
+	}
+
+}
