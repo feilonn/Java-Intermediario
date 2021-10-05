63535b19f75dcbb8a03a024df71db2c440a2b7be
Raul Correia <raulvinicius245@gmail.com>
2021-10-04 22:16:35 -0300

    Instalando jars no proejto.

---
+++ src/com/raul/javabasico/aula85_100/Aula100.java
@@ -0,0 +1,13 @@
+package com.raul.javabasico.aula85_100;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Aula100 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(StringUtils.capitalize("raul"));
+
+	}
+
+}
